599649b91925d285921c5c57be18fd9f70087f4a
jzb0228 <156571798+jzb0228@users.noreply.github.com>
2024-02-25 03:10:09 -0800

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Justin Baik on 2/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
